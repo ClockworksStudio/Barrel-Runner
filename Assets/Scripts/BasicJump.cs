505aec4a3fedd0643394aaa50084f8cfd4b4b32d
Matias Jensen <main@clockworksstudio.com>
2014-08-11 16:07:15 +0200

    Added a scene with a jumping object

---
+++ Assets/Scripts/BasicJump.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicJump : MonoBehaviour
+{
+	public float jumpSpeed = 300.0f;
+
+	private bool onGround = true;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) && onGround == true)
+		{
+			Jump();
+		}
+	}
+	void Jump()
+	{
+		rigidbody.AddForce(Vector3.up * jumpSpeed);
+		onGround = false;
+	}
+	void OnTriggerEnter()
+	{
+		onGround = true;
+	}
+}
