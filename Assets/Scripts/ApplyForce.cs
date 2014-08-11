7ca6f3e2896f274fdd743c96fd712f807fe1c11f
Matias Jensen <main@clockworksstudio.com>
2014-08-11 17:09:06 +0200

    Basic spawn and move scripts for what is eventually going to be crayons

---
+++ Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyForce : MonoBehaviour
+{
+	public float moveSpeed = 10.0f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position += (Vector3.left * moveSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/SpawnCrayon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCrayon : MonoBehaviour {
+
+	public GameObject crayonObject;
+	public float minHorizontal = -10.0f;
+	public float maxHorizontal = 10.0f;
+	public float minVertical = -10.0f;
+	public float maxVertical = 10.0f;
+	public float minSpawnTime = 1.0f;
+	public float maxSpawnTime = 1.0f;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Invoke ("Spawn", Random.Range(minSpawnTime, maxSpawnTime));
+	}
+	
+	void Spawn()
+	{
+		Instantiate(crayonObject, transform.position + new Vector3(Random.Range(minHorizontal,maxHorizontal),Random.Range(minVertical,maxVertical)), Quaternion.identity);
+		Invoke ("Spawn", Random.Range(minSpawnTime, maxSpawnTime));
+	}
+}
