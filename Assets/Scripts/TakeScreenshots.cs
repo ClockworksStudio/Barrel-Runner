6f71cceb99138ecb2badfb8819b86c2bdebfaa3b
Matias Jensen <main@clockworksstudio.com>
2014-08-25 16:33:39 +0200

    Added backgrounds and fixed animations.

---
+++ Assets/Scripts/TakeScreenshots.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TakeScreenshots : MonoBehaviour
+{
+	public string screenshotKey = "p";
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKeyDown(screenshotKey))
+		{
+			Application.CaptureScreenshot("Screenshot" + System.DateTime.Now.ToString("yyMMdd H.mm.ss") + ".png",3);
+		}
+	}
+}
